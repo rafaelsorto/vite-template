3d20efc1eb764cc8dd2ee731cd6d289fd3032c5c
Rafael Sorto <rafaelsorto@gmail.com>
2025-02-16 14:55:48 -0600

    Select clears with null example

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+import { Button, Flex, Select } from '@mantine/core';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export function HomePage() {
+  const data = ['Mantine', 'Shadcn', 'Chakra'];
+  const [selected, setSelected] = useState<string | null>('');
+
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <Flex gap={10} mx={'auto'} justify={'center'} mt={10}>
+        <Select
+          data={data}
+          value={selected}
+          onChange={setSelected}
+          placeholder="Please select a UI"
+        />
+        <Button onClick={() => setSelected('')}>Set to empty string</Button>
+        <Button onClick={() => setSelected(null)}>Set to null</Button>
+        <pre>{JSON.stringify(selected, undefined, 2)}</pre>
+      </Flex>
     </>
   );
 }
